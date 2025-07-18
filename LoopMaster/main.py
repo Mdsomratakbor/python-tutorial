fd455987a4688f738aa62736654b18b0e5797e4e
Md Somrat Akbor <M.Samrat@surbanajurong.com>
2025-07-18 10:32:40 +0600

    porject structure update

--- LoopMaster/main.py
+++ LoopMaster/main.py
@@ -4,7 +4,6 @@ import loop_controls
 import nested_loops
 import loop_collections
 import shape_print
-import number_guess_game
 
 def run_all():
     print("=== LoopMaster: Looping Demo in Python ===\n")
@@ -14,6 +13,5 @@ def run_all():
     nested_loops.run()
     loop_collections.run()
     shape_print.run()
-    number_guess_game.run()
 if __name__ == "__main__":
     run_all()
---
+++ games/main.py
@@ -0,0 +1,7 @@
+import number_guess_game as ngg
+
+def run_all():
+    ngg.run()
+
+if __name__ == "__main__":
+    run_all()
